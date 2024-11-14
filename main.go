063e98181646b1ccf15b3a96a60cfd7ace980126
Keremimo <nyaa@live.com>
2024-11-14 19:07:52 +0100

    Add credentials and initialize logic

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/term"
+	"os"
+	"syscall"
+)
+
+type Credentials struct {
+	Username string
+	Password string
+	Email    string
+}
+
+func InitializeCredentials(c *Credentials) error {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter username: ")
+	username, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	c.Username = username
+	fmt.Println("Enter your e-mail address: ")
+	email, err := reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	c.Email = email
+	fmt.Println("Input a password")
+	password, err := term.ReadPassword(syscall.Stdin)
+	if err != nil {
+		return err
+	}
+	c.Password = string(password)
+	fmt.Println(c.Username)
+	fmt.Println(c.Email)
+	fmt.Println(c.Password)
+	return nil
+}
+
+func main() {
+	credentials := new(Credentials)
+	InitializeCredentials(credentials)
+}
