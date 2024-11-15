2c68d7953bde01e5522fe59fb914ea8f1f4c1baa
keremimo <nyaa@live.com>
2024-11-15 23:06:19 +0100

    Replace console output with one that works

--- execfunc/bitwardenlogin.go
+++ execfunc/bitwardenlogin.go
@@ -7,18 +7,28 @@ import (
 )
 
 func BitwardenLogin(c *credmanagement.Credentials, b *credmanagement.BwCredentials) error {
-	out, err := exec.Command("/bin/sh", "-c", "echo %s | bw config server", b.Server).Output()
-	if err != nil {
-		return err
-	}
-	fmt.Println(string(out[:]))
+	cmd := exec.Command("/bin/sh", "-c", "echo %s | bw config server", b.Server)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
 
-	out, err = exec.Command("/bin/sh", "-c", fmt.Sprintf("echo %s %s --code %s | bw login", b.Username, b.Password, b.TwoFactor)).Output()
-
+	err := cmd.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("error: %v, stderr: %s", err, stderr.String())
 	}
-	fmt.Println(string(out[:]))
+	fmt.Println(stdout.String())
+
+	cmd = exec.Command("/bin/sh", "-c", fmt.Sprintf("echo %s %s --code %s | bw login", b.Username, b.Password, b.TwoFactor))
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error: %v, stderr: %s", err, stderr.String())
+	}
+	fmt.Println(stdout.String())
 
 	return nil
 }
