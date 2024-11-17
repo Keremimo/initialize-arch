c6a3b617ea0392f08ec5862bb7337056ab3502b3
Keremimo <nyaa@live.com>
2024-11-17 09:37:52 +0100

    Fix bugs

--- execfunc/bitwardenlogin.go
+++ execfunc/bitwardenlogin.go
@@ -1,9 +1,11 @@
 package execfunc
 
 import (
+	"bytes"
 	"fmt"
-	"github.com/keremimo/initialize-arch/credmanagement"
 	"os/exec"
+
+	"github.com/keremimo/initialize-arch/credmanagement"
 )
 
 func BitwardenLogin(c *credmanagement.Credentials, b *credmanagement.BwCredentials) error {
@@ -20,11 +22,10 @@ func BitwardenLogin(c *credmanagement.Credentials, b *credmanagement.BwCredentia
 
 	cmd = exec.Command("/bin/sh", "-c", fmt.Sprintf("echo %s %s --code %s | bw login", b.Username, b.Password, b.TwoFactor))
 
-	var stdout, stderr bytes.Buffer
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
-	err := cmd.Run()
+	err = cmd.Run()
 	if err != nil {
 		return fmt.Errorf("error: %v, stderr: %s", err, stderr.String())
 	}
