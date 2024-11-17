5c5bac14cc155fea8dcae4a3a9fe8d9324ea6605
Keremimo <nyaa@live.com>
2024-11-17 15:18:35 +0100

    Add .env loading

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.23.2
 require golang.org/x/term v0.26.0
 
 require (
+	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/sys v0.27.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,19 +2,37 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"github.com/keremimo/initialize-arch/credmanagement"
 	"github.com/keremimo/initialize-arch/execfunc"
 )
 
 func main() {
-	credentials := new(credmanagement.Credentials)
-	bwCredentials := new(credmanagement.BwCredentials)
-	_ = bwCredentials // Placeholder
-	err := credmanagement.InitializeCredentials(credentials)
+	err := godotenv.Load()
 	if err != nil {
-		fmt.Println("Something horrible happened.")
-		fmt.Println(err)
+		log.Fatalf("Error loading .env file")
 	}
+	credentials := &credmanagement.Credentials{
+		Username:   os.Getenv("USERNAME"),
+		Password:   os.Getenv("PASSWORD"),
+		Email:      os.Getenv("EMAIL"),
+		GithubName: os.Getenv("GITHUB_NAME"),
+	}
+
+	bwCredentials := &credmanagement.BwCredentials{
+		Server:   os.Getenv("BW_SERVER"),
+		Username: os.Getenv("BW_USERNAME"),
+		Password: os.Getenv("BW_PASSWORD"),
+	}
+	_ = bwCredentials // Placeholder
+	// err = credmanagement.InitializeCredentials(credentials)
+	// if err != nil {
+	// 	fmt.Println("Something horrible happened.")
+	// 	fmt.Println(err)
+	// }
 	err = execfunc.EnableBluetooth(credentials)
 	if err != nil {
 		fmt.Println(err)
